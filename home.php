52d21a4df115a78a6898bf0632a404623a2d51a9
Sean Willess <swilless@adfero.com>
2011-07-11 14:17:17 -0400

    index.html, home.php - 7/11/2011 - swilless
    Sean Willess added some text to index.html and home.php for testing purposes.

--- home.php
+++ home.php
@@ -2,7 +2,7 @@
 <head>This is the head</head>
 <body>
 	<h1>This is some caption</h1>
-	<p>Here's some more pointless info</p>
+	<p>Here's some more pointless info, and more.</p>
 
 </body>
 </html>
